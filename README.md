8f784d0e8e9a21098173033db81fdba3775e466a
ferrgabe <ferrgabe@outlook.com>
2025-06-02 20:38:32 -0300

    Adição de novas rotas

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LedComProximidade
+
+- Separação clara de responsabilidades (MVC)
+
+- Rotas bem organizadas
+
+- Controllers focados em lidar com requisições/respostas
+ 
+- Services com a lógica de negócio
+
+- Models para interação com o banco de dados
+
+- Utilitários compartilhados
--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const deviceController = require('../controllers/deviceController');
 
+// Rotas para dispositivos
 router.get('/', deviceController.getAllDevices);
 router.get('/active', deviceController.getActiveDevices);
+router.get('/:deviceId', deviceController.getDeviceById);
+router.get('/:deviceId/history', deviceController.getDeviceHistory);
 router.post('/:deviceId/command', deviceController.sendCommand);
 router.post('/:deviceId/ping', deviceController.pingDevice);
+router.put('/:deviceId', deviceController.updateDevice);
 
 module.exports = router;
